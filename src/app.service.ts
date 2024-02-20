51f9a0385d042d0642bfabdcbb08b6850567a16f
william.barbosa <william.barbosa>
2024-02-19 21:15:53 -0500

    Imprime URL STRING

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
+  constructor(private readonly configService: ConfigService) {
+    console.log('constructor');
+    console.log(process.env.DATABASE_URL);
+    console.log(this.configService.get('DATABASE_URL'));
+  }
   getHello(): string {
+    console.log('getHello');
+    console.log(process.env.DATABASE_URL);
+    console.log(this.configService.get('DATABASE_URL'));
     return 'URL: ' + process.env.DATABASE_URL;
   }
 }
